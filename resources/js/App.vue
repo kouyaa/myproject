95ae3f66c424e57e6fe8a0ddcf69259b7416c749
Your Namekouyaa <kouyaa2021@gmail.com>
2023-10-24 20:58:22 +0700

    first commit-upload

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h3>hello vue app</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProjectApp',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resource/css/app.css')
+</head>
+<body>
+    <h2> hello laravel app</h2>
+    @vite('resource/js/app.js')
+    <hr>
+      
+      <div id="app.vue">
+          hello view
+      </div>
+</body>
+</html>
